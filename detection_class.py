08d84ec04c7f45c4d95c5e08681e6e1ccf20b63e
Aditya J <aditya.storm@gmail.com>
2018-11-19 14:58:41 +0530

    Written Detection class

---
+++ detection_class.py
@@ -0,0 +1,41 @@
+import cv2
+
+
+class Detection:
+    def __init__(self, bbox):
+        self.bbox = bbox  # [x_min, y_min, x_max, y_max] the detection list
+        self.label = None  # an id to mark the detection box
+        self.is_marked = False  # true if box is labelled and false otherwise
+
+    def draw_bbox(self, frame):
+        x_min, y_min, x_max, y_max = self.bbox
+
+        if self.label is None:
+            # draw a thin white box
+            colour = (255, 255, 255)
+            box_thickness = 1
+            cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), colour, box_thickness)
+
+        else:
+            # draw a slightly thicker red box
+            colour = (0, 0, 255)
+            box_thickness = 2
+            cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), colour, box_thickness)
+
+            # mention label on the box
+            label_text = '{}'.format(self.label)
+            text_colour = (255, 255, 255)
+            text_size = 0.5
+            cv2.putText(frame, label_text, (x_min, y_min - 5), cv2.FONT_HERSHEY_SIMPLEX, text_size,
+                        text_colour, 1, cv2.LINE_AA)
+
+    def point_in_box(self, point):
+        """ returns the clicked region of interest
+        params: point - a tuple consisting of the mouse event pixel coordinate
+        return: True/False - a boolean"""
+        (x, y) = point
+        x_min, y_min, x_max, y_max = self.bbox
+        if x_min < x < x_max and y_min < y < y_max:
+            return True
+        else:
+            return False
